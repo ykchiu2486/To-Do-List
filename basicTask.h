1be7a0edbf9d22373117f1b8e8f38b6d3145959e
ykc2486 <roger2486.cs13@nycu.edu.tw>
2025-03-24 18:13:32 +0800

    first commit

---
+++ basicTask.h
@@ -0,0 +1,13 @@
+#ifndef BASICTASK_H
+#define BASICTASK_H
+
+//#include <iostream>
+#include <string>
+
+class Basic_task {
+    std::string *name;
+    std::string *category;
+    bool *completed;
+};
+
+#endif
